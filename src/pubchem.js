de87892d25222da4db6c4a3b85fb9269727094c6
Michaël Zasso <targos@protonmail.com>
2016-06-06 21:06:50 +0200

    cleanup

--- src/pubchem.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const program = require('commander');
-const pkg = require('../package.json');
-
-program
-    .version(pkg.version)
-    .command('download', 'download the complete current database')
-    .parse(process.argv);
