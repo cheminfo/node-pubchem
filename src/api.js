7336110e3233ee3fd7e87febfa375ebdb5b582b1
Michaël Zasso <targos@protonmail.com>
2016-06-28 14:10:04 +0200

    start working on searchEm

---
+++ src/api.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const mongo = require('mongo');
+const co = require('co');
+const dataCollection = mongo.connect().then(db => db.collection('data'));
+
+const defaultSearchEmOptions = {
+    limit: 1e3,
+    resolution: 1e6
+};
+const searchEm = co.wrap(function* em(value, options) {
+    if (typeof value !== 'number') {
+        throw new Error('value must be a number');
+    }
+    options = Object.assign({}, defaultSearchEmOptions, options);
+
+    if (options.limit > 1e4) options.limit = 1e4;
+    if (options.limit < 1) options.limit = 1;
+
+    let error;
+    if (options.accuracy) { // accuracy is in ppm
+        error = value / 1e6 * options.accuracy;
+    } else {
+        error = value / options.resolution;
+    }
+
+    const data = yield dataCollection;
+    const searchBottom = data.find({em: {$lte: value, $gte: value - error}}).sort({em: -1}).limit(options.limit).project({'ocl.id': 1, mf: 1, em: 1}).toArray();
+    const searchTop = data.find({em: {$gte: value, $lte: value + error}}).sort({em: 1}).limit(options.limit).project({'ocl.id': 1, mf: 1, em: 1}).toArray();
+    const [resultBottom, resultTop] = yield Promise.all([searchBottom, searchTop]);
+
+    console.log('bottom');
+    console.log(resultBottom);
+    console.log('top');
+    console.log(resultTop);
+});
+
+exports.search = {
+    searchEm
+};
