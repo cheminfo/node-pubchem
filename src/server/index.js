927f4a3b147426246ec7ccc43cd146e9cdc1377e
Michaël Zasso <targos@protonmail.com>
2016-06-28 14:42:44 +0200

    add server and searchEm route

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const koa = require('koa');
+const kcors = require('kcors');
+const router = require('./router');
+
+const hasPort = process.argv.indexOf('--port');
+if (hasPort === -1) throw new Error('missing port option');
+const PORT = parseInt(process.argv[hasPort + 1]);
+if (!PORT) throw new Error('wrong port option: ' + process.argv[hasPort + 1]);
+
+const app = koa();
+
+app.use(kcors());
+app.use(router.routes());
+
+app.listen(PORT);
---
+++ src/server/router.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const api = require('api');
+
+const router = require('koa-router')();
+
+router.get('/search/em', function*() {
+    const value = +this.query.value;
+    if (!value) {
+        return error(this, 'missing value');
+    }
+    this.body = yield router.search.em(value, this.query);
+});
+
+function error(ctx, message) {
+    ctx.status = 400;
+    ctx.body = {
+        error: message
+    };
+}
+
+module.exports = router;
