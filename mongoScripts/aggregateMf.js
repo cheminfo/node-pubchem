8156bca7eb07664439d46059e60d27c064be309e
Michaël Zasso <targos@protonmail.com>
2016-09-07 10:08:36 +0200

    ignore mfs with a charge

--- mongoScripts/aggregateMf.js
+++ mongoScripts/aggregateMf.js
@@ -4,7 +4,7 @@ var data = db.data;
 
 data.aggregate([
 //    { $limit: 1e4 },
-    { $match: { nbFragments: 1 } },
+    { $match: { nbFragments: 1, mf: { $not: /(\+|-)\d*$/ } } },
     { $project: { mf: 1, em: 1, unsat: 1, atom: 1 } },
     { $group: { _id: '$mf', count: { $sum: 1 }, em: { $first: '$em' }, unsaturation: { $first: '$unsat'}, atom: { $first: '$atom' } } },
     { $out: 'aggregateMf' }
