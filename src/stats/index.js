35a69999f7e673088c99fc7c5851b889859af6a3
Luc Patiny <luc@patiny.com>
2017-01-05 17:12:58 +0100

    Improve the storage in the database https://github.com/cheminfo/node-pubchem/commit/034b1a5e6f935b8f1df1a2758fc38e6c753b08db#commitcomment-20373811

--- src/stats/index.js
+++ src/stats/index.js
@@ -64,21 +64,13 @@ co(function*() {
     // we will save the result in the collection 'stats'
     var id = result.options.stepMass + "_" + result.options.elementRatios.join('.').replace(/\//g,'-');
     const statsCollection = db.collection('mfStats');
-    let statsEntry = yield statsCollection.find({_id: id}).next();
-    if (statsEntry === null) {
-        statsEntry = {
-            _id: id,
-            date: new Date(),
-            options: result.options,
-            stats: result.results
-        };
-        yield statsCollection.insertOne(statsEntry);
-    } else {
-        statsEntry.date = new Date();
-        statsEntry.options = result.options;
-        statsEntry.stats = result.results;
-        yield statsCollection.replaceOne({_id: statsEntry._id}, statsEntry);
-    }
+    let statsEntry = {
+        _id: id,
+        date: new Date(),
+        options: result.options,
+        stats: result.results
+    };
+    yield statsCollection.replaceOne({_id: statsEntry._id}, statsEntry, {upsert: true});
     console.log(`Statistics saved as ${id} in collection mfStats`);
 
    // console.log(JSON.stringify(result, null, 2));
